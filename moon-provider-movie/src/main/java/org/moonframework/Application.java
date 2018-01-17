0e0a535802a9ee747b5748cc6f08bec6cc8447ce
helldzl <1949602@qq.com>
2018-01-17 14:39:54 +0800

    zuul

--- moon-provider-movie/src/main/java/org/moonframework/Application.java
+++ moon-provider-movie/src/main/java/org/moonframework/Application.java
@@ -2,19 +2,17 @@ package org.moonframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 /**
  *
  */
+// @EnableCircuitBreaker
+// @EnableHystrix
+// @EnableHystrixDashboard
 @EnableFeignClients
 @EnableDiscoveryClient
-@EnableCircuitBreaker
-// @EnableHystrix
-@EnableHystrixDashboard
 @SpringBootApplication
 public class Application {
 
