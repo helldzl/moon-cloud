6458318056560fd7100241eecba0daa81dd7ad4c
helldzl <1949602@qq.com>
2018-02-03 10:03:33 +0800

    config

--- moon-provider-user/src/main/java/org/moonframework/controller/UserController.java
+++ moon-provider-user/src/main/java/org/moonframework/controller/UserController.java
@@ -1,9 +1,12 @@
 package org.moonframework.controller;
 
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.moonframework.entity.User;
 import org.moonframework.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,8 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
-    // protected final Logger logger = LogManager.getLogger(UserController.class);
-    private static final Log logger = LogFactory.getLog(UserController.class);
+    // private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final Log logger = LogFactory.getLog(this.getClass());
 
     @Autowired
     private UserRepository userRepository;
